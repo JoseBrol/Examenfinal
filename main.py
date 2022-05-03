605b54e5b106ecd944f084ad2e09ab7cda410fd7
José Brol <jabrolc10@gmail.com>
2022-05-03 09:29:19 -0600

    Actualización de archivo .py
    Se hizo una actualización del archivo main.py, se agrego un programa de animal favorito, en la rama develop.

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ print("Nombre: José Andrés Brol Cuevas")
 print("Carné: 1577222")
 print("Curso: Introducción a la ingenieria")
 print("Ciclo: 2022")
+
+
+#Programa del animal favorito
+
+animal = input("Si su animal favorito es el perro ingrese 1, si es otro ingrese el nombre de tu animal favorito: ")
+if animal == 1:
+    print("Mi animal favorito también es el perro")
+else:
+    print(f"Oooh, tu {animal} es interesante")
+
+
+
+    
